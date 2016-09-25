708413635f709540fbd4a607825d7e93f170e1f9
王恒尊 <982049377@qq.com>
2016-09-25 15:23:16 +0800

    no message

--- out/main.js
+++ out/main.js
@@ -1,23 +1,36 @@
-var Greeter = (function () {
-    function Greeter(element) {
-        this.element = element;
-        this.element.innerHTML += "The time is: ";
-        this.span = document.createElement('span');
-        this.element.appendChild(this.span);
-        this.span.innerText = new Date().toUTCString();
+// TypeScript file
+var Row; //行数
+var StarArray; //星星存储矩阵
+var TempColomn; //临时列数
+var TempRow; //临时行数
+var Colomn; //行数
+StarArray = [[]];
+Row = 5;
+Colomn = 2 * Row - 1;
+initialize();
+set();
+output();
+function initialize() {
+    StarArray.pop();
+    for (TempRow = 0; TempRow < Row; TempRow++) {
+        var TempArray = [];
+        for (TempColomn = 0; TempColomn < Colomn; TempColomn++)
+            TempArray.push(" ");
+        StarArray.push(TempArray);
     }
-    Greeter.prototype.start = function () {
-        var _this = this;
-        this.timerToken = setInterval(function () { return _this.span.innerHTML = new Date().toUTCString(); }, 500);
-    };
-    Greeter.prototype.stop = function () {
-        clearTimeout(this.timerToken);
-    };
-    return Greeter;
-}());
-window.onload = function () {
-    var el = document.getElementById('content');
-    var greeter = new Greeter(el);
-    greeter.start();
-};
+}
+function set() {
+    for (TempColomn = 0; TempColomn < Row; TempColomn++) {
+        StarArray[TempColomn][(Colomn - 1) / 2] = "*";
+        for (TempRow = 0; TempRow <= TempColomn; TempRow++) {
+            StarArray[TempColomn][(Colomn - 1) / 2 - TempRow] = "*";
+            StarArray[TempColomn][(Colomn - 1) / 2 + TempRow] = "*";
+        }
+    }
+}
+function output() {
+    for (TempColomn = 0; TempColomn < Row; TempColomn++) {
+        console.log(StarArray[TempColomn]);
+    }
+}
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,47 @@
-class Greeter {
-    element: HTMLElement;
-    span: HTMLElement;
-    timerToken: number;
+// TypeScript file
 
-    constructor(element: HTMLElement) {
-        this.element = element;
-        this.element.innerHTML += "The time is: ";
-        this.span = document.createElement('span');
-        this.element.appendChild(this.span);
-        this.span.innerText = new Date().toUTCString();
+var Row:number;//行数
+var StarArray:string[][];//星星存储矩阵
+var TempColomn:number;//临时列数
+var TempRow:number;//临时行数
+var Colomn:number;//行数
+StarArray=[[]];
+Row=5;
+Colomn=2*Row-1;
+initialize()
+set();
+output();
+
+function initialize():void
+{
+    StarArray.pop();
+    for(TempRow=0;TempRow<Row;TempRow++)
+    {
+        var TempArray=[];
+        for(TempColomn=0;TempColomn<Colomn;TempColomn++)
+            TempArray.push(" "); 
+        StarArray.push(TempArray);
     }
-
-    start() {
-        this.timerToken = setInterval(() => this.span.innerHTML = new Date().toUTCString(), 500);
-    }
-
-    stop() {
-        clearTimeout(this.timerToken);
-    }
-
 }
 
-window.onload = () => {
-    var el = document.getElementById('content');
-    var greeter = new Greeter(el);
-    greeter.start();
-};
+function set():void
+{
+    for(TempColomn=0;TempColomn<Row;TempColomn++)
+    {  
+        StarArray[TempColomn][(Colomn-1)/2]="*";
+        for(TempRow=0;TempRow<=TempColomn;TempRow++)
+        {
+            StarArray[TempColomn][(Colomn-1)/2-TempRow]="*";
+            StarArray[TempColomn][(Colomn-1)/2+TempRow]="*";
+        }
+    }
+            
+}
+
+function output():void
+{
+     for(TempColomn=0;TempColomn<Row;TempColomn++)
+    {
+            console.log(StarArray[TempColomn]);
+    }
+}
