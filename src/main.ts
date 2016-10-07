050413d5c44c5093836be3d61261ee2b286b6ebd
王恒尊 <982049377@qq.com>
2016-10-07 20:48:45 +0800

    no message

--- src/main.ts
+++ src/main.ts
@@ -1,47 +1,50 @@
 // TypeScript file
 
 var Row:number;//行数
-var StarArray:string[][];//星星存储矩阵
-var TempColomn:number;//临时列数
-var TempRow:number;//临时行数
-var Colomn:number;//行数
-StarArray=[[]];
-Row=5;
-Colomn=2*Row-1;
-initialize()
-set();
-output();
-
-function initialize():void
+function print(Row ):void
 {
-    StarArray.pop();
-    for(TempRow=0;TempRow<Row;TempRow++)
+    var StarArray:string[][];//星星存储矩阵
+    var TempColomn:number;//临时列数
+    var TempRow:number;//临时行数
+    var Colomn:number;//行数
+    StarArray=[[]];
+    Row=5;
+    Colomn=2*Row-1;
+    initialize()
+    set();
+    output();
+
+    function initialize():void
     {
-        var TempArray=[];
-        for(TempColomn=0;TempColomn<Colomn;TempColomn++)
-            TempArray.push(" "); 
-        StarArray.push(TempArray);
-    }
-}
-
-function set():void
-{
-    for(TempColomn=0;TempColomn<Row;TempColomn++)
-    {  
-        StarArray[TempColomn][(Colomn-1)/2]="*";
-        for(TempRow=0;TempRow<=TempColomn;TempRow++)
+        StarArray.pop();
+        for(TempRow=0;TempRow<Row;TempRow++)
         {
-            StarArray[TempColomn][(Colomn-1)/2-TempRow]="*";
-            StarArray[TempColomn][(Colomn-1)/2+TempRow]="*";
+            var TempArray=[];
+            for(TempColomn=0;TempColomn<Colomn;TempColomn++)
+                TempArray.push(" "); 
+            StarArray.push(TempArray);
         }
     }
-            
-}
 
-function output():void
-{
-     for(TempColomn=0;TempColomn<Row;TempColomn++)
+    function set():void
     {
-            console.log(StarArray[TempColomn]);
+        for(TempColomn=0;TempColomn<Row;TempColomn++)
+        {  
+            StarArray[TempColomn][(Colomn-1)/2]="*";
+            for(TempRow=0;TempRow<=TempColomn;TempRow++)
+            {
+                StarArray[TempColomn][(Colomn-1)/2-TempRow]="*";
+                StarArray[TempColomn][(Colomn-1)/2+TempRow]="*";
+            }
+        }
+                
+    }
+
+    function output():void
+    {
+        for(TempColomn=0;TempColomn<Row;TempColomn++)
+        {
+                console.log(StarArray[TempColomn]);
+        }
     }
 }
